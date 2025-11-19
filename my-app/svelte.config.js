a99a1a81f22864bb4b3a7d676bb2eadfc83ddd81
Web4 <167559384+Web4application@users.noreply.github.com>
2025-11-20 00:49:21 +0100

    Create svelte.config.js

---
+++ my-app/svelte.config.js
@@ -0,0 +1,9 @@
+// svelte.config.js
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: [vitePreprocess()]
+};
+
+export default config;
